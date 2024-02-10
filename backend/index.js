be0efbf4f1b860cfcaf118005f60de2d4df22697
sanjaykumarmsk <129680092+sanjaykumarmsk@users.noreply.github.com>
2024-02-11 00:41:00 +0530

    Initial commit

---
+++ backend/index.js
@@ -0,0 +1,26 @@
+const port = 4000;
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
+const multer = require("multer");
+const path = require("path");
+const cors = require("cors");
+
+app.use(express.json());
+app.use(cors());
+
+// Database connection with mongodb
+mongoose.connect(
+  "mongodb+srv://shopvista:shopvista@2024@cluster0.cuavhdq.mongodb.net/e-commerce"
+);
+
+// API Creation
+
+app.listen(port, (error) => {
+  if (!error) {
+    console.log("Server Running on Port " + port);
+  } else {
+    console.log("Error : " + error);
+  }
+});
